5e8762ea374134e096ade548f9d866d53b6b4b47
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-07-12 02:40:43 +0200

    Rust animate

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# reveal.js-eliminate-emtpy-lines
+# reveal.js-animate
 
 A [Reveal.js](https://revealjs.com/) plugin to eliminate line breaks in &lt;pre&gt; tags in lines that don't contain anything other than an html tag.
 
 ## Installation
 
 ```bash
-npm i reveal.js-eliminate-emtpy-lines
+npm i reveal.js-animate
 ```
 
 ## Usage
@@ -14,12 +14,12 @@ Add the plugin:
 
 ```html
   <script type="module">
-    import RevealEliminateEmtpyLines from "reveal.js-eliminate-empty-lines";
+    import RevealAnimate from "reveal.js-animate";
 
     import Reveal from "reveal.js";
 
     let deck = new Reveal({
-      plugins: [RevealEliminateEmtpyLines],
+      plugins: [RevealAnimate],
     });
     deck.initialize();
   </script>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 export default () => ({
-  id: "fragments-rust",
+  id: "animate",
   init: (deck) => {
-    const codes = document.querySelectorAll("code.rust");
+    const codes = document.querySelectorAll(".balanced");
 
     for (const code of codes) {
       code.innerHTML = code.innerHTML.replace(
-        /^([^{\n]*){$/gm,
-        '<span class="fragment fade-in">$1{'
+        /^(.*){(.*)$/gm,
+        '<span class="fragment fade-in">$1{$2'
       );
-      code.innerHTML = code.innerHTML.replace(/}/gm, "}</span>");
-      code.innerHTML = code.innerHTML.replace(/^([^{}\n]*)$/gm, '<span class="fragment fade-in">$1</span>');
+      code.innerHTML = code.innerHTML.replace(/}(.*)$/gm, "}$1</span>");
+      code.innerHTML = code.innerHTML.replace(
+        /\(([^\)])/gm,
+        '(<span class="fragment fade-in">$1'
+      );
+      code.innerHTML = code.innerHTML.replace(/([^\(\)])\)([^\)])/gm, "$1</span>)$2");
+      code.innerHTML = code.innerHTML.replace(/([^\(])\)\)/gm, "$1</span>)</span>)");
+      code.innerHTML = code.innerHTML.replace(/\(\)\)/gm, "()</span>)");
+    }
+
+    const byLines = document.querySelectorAll(".by-line");
+
+    for (const byLine of byLines) {
+      byLine.innerHTML = byLine.innerHTML.replace(
+        /^(\ *(?:[^{} \n]+) *[^{}\n]*)$/gm,
+        '<span class="fragment fade-in">$1</span>'
+      );
     }
   },
 });
